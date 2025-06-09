3e03de6b5de4630f116047b609b89970e587804b
Dmitriy Baranov <dmitriy@laravel.com>
2025-06-09 13:58:43 -0400

    add assetlinks.json route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/.well-known/assetlinks.json', function () {
+    return response()->json([
+        [
+            "relation" => ["delegate_permission/common.handle_all_urls"],
+            "target" => [
+                "namespace" => "android_app",
+                "package_name" => "com.example.test",
+                "sha256_cert_fingerprints" => [
+                    "E5:D7:C9"
+                ]
+            ]
+        ],
+        [
+            "relation" => ["delegate_permission/common.handle_all_urls"],
+            "target" => [
+                "namespace" => "android_app",
+                "package_name" => "com.example.test.preview",
+                "sha256_cert_fingerprints" => [
+                    "66:93"
+                ]
+            ]
+        ],
+        [
+            "relation" => ["delegate_permission/common.handle_all_urls"],
+            "target" => [
+                "namespace" => "android_app",
+                "package_name" => "com.example.test.dev",
+                "sha256_cert_fingerprints" => [
+                    "AC:FF:AD"
+                ]
+            ]
+        ]
+    ]);
+});
