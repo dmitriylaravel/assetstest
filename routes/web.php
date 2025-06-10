22add74a025a450984fbed3b612bf2218bb829be
Dmitriy Baranov <dmitriy@laravel.com>
2025-06-10 13:58:23 -0400

    wip

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/.well-known/assetlinks.json', function () {
+    return response()->json([
+        [
+            "relation" => ["delegate_permission/common.handle_all_urls"],
+            "target" => [
+                "namespace" => "android_app",
+                "package_name" => "com.example.test",
+                "sha256_cert_fingerprints" => [
+                    "E5:D7:C9"
+                ]
+            ]
+        ]
+    ]);
+});
